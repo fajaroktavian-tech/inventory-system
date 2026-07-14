84ad1cf4c7356473f23406374fa8589a19ca8767
Fajar Oktavian Nugraha <fajaroktavian58@gmail.com>
2026-07-14 08:38:30 +0700

    Fix Alpinejs declaration conflict in app.js

--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-import { Alpine, Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
-
-// Pastikan Alpine diinisialisasi
-import Alpine from 'alpinejs';
-window.Alpine = Alpine;
-Alpine.start();
